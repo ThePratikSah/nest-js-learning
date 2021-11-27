c3faca379db982b1e685c7a524a1743f880838de
deqodepratik <psah@deqode.com>
2021-11-27 14:29:14 +0530

    Created DTO Classes and added params and body in both get and post request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { GetAll } from './dto/get-all.dto';
 
 @Controller()
 export class AppController {
@@ -7,17 +9,19 @@ export class AppController {
 
   // logic for post request
   @Post()
-  create(): object {
-    return { msg: 'New post request reseve!' };
+  create(@Body() body: CreatePostDto): object {
+    return { msg: 'New post request!', name: body.name, age: body.age };
   }
 
   // logic for get request
-  @Get('ping')
   // this decorator will help us to return different status code
+  @Get('ping/:name/:age')
   @HttpCode(202)
-  getHello(): object {
+  getHello(@Param() reqParam: GetAll): object {
     return {
       msg: this.appService.getHello(),
+      name: reqParam.name,
+      age: reqParam.age,
     };
   }
 }
---
+++ src/dto/get-all.dto.ts
@@ -0,0 +1,4 @@
+export class GetAll {
+  name: string;
+  age: number;
+}
